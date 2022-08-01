f3d895e9311b616ee72d0884435dd55de30b1df0
再见田园犬 <tcstory@163.com>
2022-08-01 11:32:51 +0800

    feat: init.lua

---
+++ init.lua
@@ -0,0 +1,15 @@
+vim.cmd('syntax enable')
+
+vim.opt.number = true
+vim.opt.list = true
+vim.opt.listchars = 'extends:❯,precedes:❮,tab:▸ ,trail:˽'
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 1000
+vim.opt.clipboard = vim.opt.clipboard + 'unnamedplus'
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.cursorline = true
