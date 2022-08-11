5c474506291e5d6b97fa13f7c4c75747a9802f22
再见田园犬 <tcstory@163.com>
2022-08-11 16:15:36 +0800

    feat: 添加 packer

--- init.lua
+++ init.lua
@@ -15,6 +15,9 @@ vim.opt.expandtab = true
 vim.opt.cursorline = true
 
 require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
   use 'h-hg/fcitx.nvim'
   use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
   use 'kyazdani42/nvim-web-devicons'
